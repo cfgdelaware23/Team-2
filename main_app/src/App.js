67fad20bad3c110138e73678d4211f0635a196d8
Shashank Saini <sainishashank@aol.com>
2023-09-29 21:30:11 -0400

    Added ARIA ((Accessible Rich Internet Applications) Attributes

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -8,22 +8,23 @@ function App() {
   const handleDrop = (e) => {
     e.preventDefault();
     const droppedFile = e.target.files[0];
+    //Selecting file is possible with just keyboard
     setFile(droppedFile);
     //apiLayer(file);
   };
 
-  /*const handleDefaultDrag = (e) => {
-    e.preventDefault();
-  }*/
-
+  //"Label" semantic HTML tag
+  //ARIA (Accessible Rich Internet Applications) Attributes. "Aria Label" provides extra information to screen reader users.
   return (
     <div className="App">
       <header className="App-header">
         {
           file ? <p>File dropped: {file.name}</p> : 
           <>
-            <p>Select an Excel file here</p>
-            <input type = "file" onChange = {handleDrop} />
+            <label htmlFor="fileInput">
+              Use the Browse button to select an Excel file from your device:
+              <input type="file" id="fileInput" onChange={handleDrop} aria-describedby="fileInput" />
+            </label>
           </>
         }
       </header>
