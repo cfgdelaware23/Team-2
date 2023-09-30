52b7eca0f7ae2022854a41bcd6ec4d5fbff37f84
jasonlewcodes <jiajie1224@gmail.com>
2023-09-29 20:12:30 -0400

    Checkpoint to make my code visible to teammates

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/getSchedule")
+@app.route("/getSchedule", methods=['POST'])
 def hello_world():
+    content = request.json
+    print(content)
     return {
         "message": "Hello, World!"
     }
--- main_app/src/api.js
+++ main_app/src/api.js
@@ -1,5 +1,20 @@
 function apiLayer() {
-  fetch("/getSchedule").then((res) =>
+  const sampleData = [
+    {
+      "name": "Jane Doe",
+      "monAvailability": "4pm-6pm",
+      "tuesAvailability": "4pm-6pm",
+      "wedAvailability": "4pm-6pm",
+      "thuAvailability": "4pm-6pm",
+      "friAvailability": "4pm-6pm",
+    }
+  ];
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(sampleData)
+  };
+  fetch("/getSchedule", requestOptions).then((res) =>
       res.json().then((data) => {
           console.log(data);
       })
