58c0c623aa787ec13e156bfa5a67cf97200ae1a2
Anna Dai <anna.dai916@gmail.com>
2023-09-29 20:43:10 -0400

    Revert to previous data structure format with volunteers and events

--- backend/schedule_builder.py
+++ backend/schedule_builder.py
@@ -1,9 +1,11 @@
 # in the format key=name, value=[{set of skills}, {key=day-of-week, value=[set of available hours in 24-hour format]]
 volunteers = {"Anna": 
                 [
-                    {"skill1"},
-                    {"Monday": [1,2,3,4]}
-                ]}
+                    {"skill1", "skill2"},
+                    {"Monday": [1,2,3,4],
+                     "Tuesday": [2,3,6,7]}
+                ],
+                }
 
 # in the format key=event, value=[{set of skills}, day-of-week, [list of hours in 24-hour format], # of volunteers]
 events = {"event1":
@@ -21,6 +23,7 @@ def list_of_volunteers_for_event(event):
 
     # Check if the event is in the set of events
     if event in events:
+        # Isolate part of the event data into variables
         event_info = events[event]
         num_volunteers = event_info[3]
         skills_needed = event_info[0]
@@ -31,12 +34,21 @@ def list_of_volunteers_for_event(event):
             volunteer = all_volunteers.pop()
             # Isolate the array of available hours for that volunteer for the day
             available_hours = volunteers[volunteer][1][day]
-            # Check the volunteer is available
-            if(set(hours_needed).issubset(set(available_hours))):
-                print("Available!")
+            # Isolate the list of skills the volunteer has
+            skills = volunteers[volunteer][0]
+            # Check the volunteer is available and has matching skills
+            if ((set(hours_needed).issubset(set(available_hours))) 
+                and skills_needed.issubset(skills)):
+                print(f"Matched {volunteer} to event {event}")
+                selected_volunteers.append(volunteer)
+                remove_hours_from_volunteer(volunteer, day, hours_needed)
+                # Decrement counter
+                num_volunteers -= 1
+            # Volunteer is not a good match
             else:
-                print("Not available!")
-
+                pass
+    
+    return selected_volunteers
 '''
 Pseudocode:
 goal: return a list of volunteers for the event and remove those hours for the volunteers
@@ -61,7 +73,10 @@ def remove_hours_from_volunteer(name, day, hours):
         volunteers[name][1][day] = list(set(volunteers[name][1][day]) - set(hours))
 
 def main():
-    list_of_volunteers_for_event("event1")
+    print(volunteers)
+    selected_volunteers = list_of_volunteers_for_event("event1")
+    print(volunteers)
+    print(selected_volunteers)
 
 if __name__ == "__main__":
     main()
