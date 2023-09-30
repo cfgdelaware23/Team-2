c528222ff28b8c4dee215efd87d79940ed649a58
Shashank Saini <sainishashank@aol.com>
2023-09-29 20:09:33 -0400

    Prevents automatic download of excel file. Now displays the file's name

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -5,12 +5,17 @@ function App() {
   const [file, setFile] = useState(null);
 
   const handleDrop = (e) => {
+    e.preventDefault();
     const droppedFile = e.dataTransfer.files[0];
     setFile(droppedFile);
   };
 
+  const handleDefaultDrag = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div className="App" onDrop={handleDrop}>
+    <div className="App" onDrop={handleDrop} onDragOver={handleDefaultDrag}>
       <header className="App-header">
         {file ? <p>File dropped: {file.name}</p> : <p>Drag and drop an Excel file here</p>}
       </header>
