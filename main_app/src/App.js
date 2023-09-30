30783ef5179770f6df86a8d128680d51617d9e8c
Shashank Saini <sainishashank@aol.com>
2023-09-30 01:49:35 -0400

    Added function to automatically download CSV. Once it is returned from the backend.

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -53,9 +53,24 @@ function App() {
 
   function speak(message) {
     const synth = window.speechSynthesis;
-    const utterance = new SpeechSynthesisUtterance(message);
-    synth.speak(utterance);
-  }  
+    const sound = new SpeechSynthesisUtterance(message);
+    synth.speak(sound);
+  }
+
+  function downloadCSV(csv, filename) {
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = filename;
+  
+    document.body.appendChild(a);
+    a.click();
+  
+    window.URL.revokeObjectURL(url);
+  }
+  
 
   //"Label" semantic HTML tag
   //ARIA (Accessible Rich Internet Applications) Attributes. "Aria Label" provides extra information to screen reader users.
