643d237843ca71aa53c9949d6ba9818a1de23c4a
Saleem Lawal <slawal1@umbc.edu>
2023-09-29 20:54:20 -0400

    added comment for readability, renamed result with index.html

--- backend/csv_html.py
+++ backend/csv_html.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import csv
+# sample csv file, will be replaced with the CSV file sent through
 csv_file = "/Users/leem/Downloads/9.17-9.23-Hosting-final.csv"
 
 app = Flask(__name__)
@@ -7,18 +8,21 @@ app = Flask(__name__)
 def read_csv(csv_file):
     dataList = []
     with open(csv_file, 'r') as file:
+        # loops through the csv file and format each row into an array index
         csv_reader = csv.reader(file)
         for row in csv_reader:
             dataList.append(row)
+    # outputs to console for debugging
+    # NEEDS TO BE TAKEN OUT LATER
     for i in range(len(dataList)):
         print(dataList[i])
-        
     return dataList
 
 @app.route('/')
 def display_csv():
+    ## sends the intended csv into the index.html file
     formatted_data = read_csv(csv_file)
-    return render_template("result.html", csv_data=formatted_data)
+    return render_template("index.html", csv_data=formatted_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
