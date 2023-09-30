022e477ed5412072466162de0f50458fbd64f41c
jasonlewcodes <jiajie1224@gmail.com>
2023-09-30 00:50:30 -0400

    Fix regresssions from merge conflict resolution

--- main_app/src/App.js
+++ main_app/src/App.js
@@ -6,10 +6,10 @@ import * as xlsx from 'xlsx';
 function App() {
   const [firstFile, setFirstFile] = useState(null);
   const [secondFile, setSecondFile] = useState(null);
+  const [firstFileName, setFirstFileName] = useState(null);
+  const [secondFileName, setSecondFileName] = useState(null);
 
   const handleDrop = (type) => (e) => {
-    e.preventDefault();
-    const droppedFile = e.target.files[0];
     const reader = new FileReader();
     reader.onload = (e) => {
       const data = e.target.result;
@@ -55,16 +55,16 @@ function App() {
       <header className="App-header">
         {
           (firstFile && secondFile) ? 
-          <p>Files selected: {firstFile.name} and {secondFile.name}</p> : 
+          <p>Files selected: {firstFileName} and {secondFileName}</p> : 
           <>
             <p>Press 'A' to select the Volunteer Availability File and 'E' to select the Event Schedule File</p>
             <label htmlFor="firstFileInput">
               Select Volunteer Availability File:&nbsp;
-              <input type="file" id="firstFileInput" onChange={handleDrop("first")} aria-describedby="fileInput" aria-label="Choose Volunteer Availability File"/>
+              <input type="file" id="firstFileInput" onDrop={handleDrop("first")} aria-describedby="fileInput" aria-label="Choose Volunteer Availability File"/>
             </label>
             <label htmlFor="secondFileInput">
               Select Event Schedule File:&nbsp;
-              <input type="file" id="secondFileInput" onChange={handleDrop("second")} aria-describedby="fileInput" aria-label="Choose Event Schedule File"/>
+              <input type="file" id="secondFileInput" onDrop={handleDrop("second")} aria-describedby="fileInput" aria-label="Choose Event Schedule File"/>
             </label>
           </>
         }
